a86e0f36f1d02f828e0f508d62ad41933c934264
Nathan Hui <ntlhui@gmail.com>
2023-10-23 15:55:03 -0700

    feat: Adds getting data directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use directories::ProjectDirs;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    job_name: String,
+
+    #[arg(short, long)]
+    working_directory: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let base_dirs = ProjectDirs::from("edu.ucsd", "e4e", "deduplicator").unwrap();
+
+    let data_dir = base_dirs.data_local_dir();
+
+    println!("{:?}", args.job_name);
+    println!("{:?}", args.working_directory);
+    println!("{:?}", data_dir)
 }
